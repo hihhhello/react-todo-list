5aff3536e70c5ab3ed8822eedf4865f34aac8ede
hihhhello <grigori.ratushniak@gmail.com>
2021-06-03 17:14:52 +0300

    Added _base.sass; Contains variables etc. Created _app.sass; added todo-list__wrapper and some styles to it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Header from "./Header";
 import ToDoListTable from "./ToDoListTable";
+import AddTaskBar from "./AddTaskBar";
+
+import "./_app.sass"
+
 function App() {
   return (
     <div className="app">
       <Header />
-      <ToDoListTable />
+      <div className="todo-list__wrapper">
+        <ToDoListTable />
+        <AddTaskBar />
+      </div>
     </div>
   );
 }
