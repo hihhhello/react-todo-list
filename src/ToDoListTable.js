e599e45532aace8b41515ceaf0e19474358b41f9
hihhhello <grigori.ratushniak@gmail.com>
2021-06-04 16:56:34 +0300

    Added onChange and onClick functions for AddTaskBar's input and button. Added local todoList for testing. Added props to ToDoList component

--- src/ToDoListTable.js
+++ src/ToDoListTable.js
@@ -3,15 +3,53 @@ import AddTaskBar from "./AddTaskBar";
 import ToDoList from "./ToDoList";
 import "./_todo-table.sass";
 
+const todoList = [
+    {descr: "Vanilla JavaScript", isDone: true},
+    {descr: "Vue.js", isDone: true},
+    {descr: "React.js", isDone: false},
+    {descr: "Node.js", isDone: false},
+];
+
 class ToDoListTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            todoTitle: '',
+        };
+
+        this.onChangeTodoTitle = this.onChangeTodoTitle.bind(this); 
+        this.addTodo = this.addTodo.bind(this); 
+    }
+
+    onChangeTodoTitle(title) {
+        this.setState({ todoTitle: title });
+    }
+
+    addTodo(e) {
+        e.preventDefault();
+        const {todoTitle} = this.state;
+        if(!todoTitle) {
+            return;
+        }
+        todoList.push({
+            descr: todoTitle,
+            isDone: false,
+        });
+        this.onChangeTodoTitle('');
+    }
+
     render() {
         return(
             <div className="todo-table">
                 <div className="container">
                     <div className="todo-table__wrapper">
                         <h1 className="todo-table__title">TO-DO LIST</h1>
-                        <AddTaskBar />
-                        <ToDoList />
+                        <AddTaskBar 
+                            onChange={this.onChangeTodoTitle}
+                            onAdd={this.addTodo}
+                            todoTitle={this.state.todoTitle}
+                        />
+                        <ToDoList todoList={todoList} />
                     </div>
                 </div>
             </div>
