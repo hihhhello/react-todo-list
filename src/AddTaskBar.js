7ad8e0a72e4f184b5be6f337f7bb855000a52e3e
hihhhello <grigori.ratushniak@gmail.com>
2021-06-04 16:59:03 +0300

    Added implementation of event's functions in props for inpput and button

--- src/AddTaskBar.js
+++ src/AddTaskBar.js
@@ -1,15 +1,24 @@
 import "./_add-task-bar.sass";
 
-function AddTaskBar() {
+function AddTaskBar({onChange, onAdd, todoTitle}) {
     return(
         <div className="add-task-bar">
             <div className="container">
                 <div className="add-task-bar__wrapper">
                     <form className="add-task-bar__form">
                         <div className="add-task-bar__input">
-                            <input placeholder="new task" />
+                            <input 
+                                value={todoTitle} 
+                                onChange={(e) => onChange(e.target.value)} 
+                                placeholder="new task"
+                            />
                         </div>
-                        <button className="add-task-bar__btn">ADD</button>
+                        <button 
+                            onClick={onAdd} 
+                            className="add-task-bar__btn"
+                        >
+                            ADD
+                        </button>
                     </form>
                 </div>
             </div>
