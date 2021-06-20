709fda718edcef3c2e42ea182207e975f8516bc3
hihhhello <grigori.ratushniak@gmail.com>
2021-06-20 23:12:39 +0300

    App js replaced with functional component with hooks

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,53 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../Header";
 import ToDoListPage from "../../pages/ToDoListPage";
 import ErrorBoundary from "../ErrorBoundary";
 
-import {
-  Redirect,
-  BrowserRouter as Router,
-} from "react-router-dom";
+import { Redirect, BrowserRouter as Router } from "react-router-dom";
 
 import { ThemeContext, themes } from "../../context/theme-context";
 
 import "./_app.sass";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+export const App = () => {
+  const [theme, setTheme] = useState(themes.light);
 
-    this.toggleTheme = () => {
-      this.setState((state) => ({
-        theme: state.theme === themes.dark ? themes.light : themes.dark,
-      }));
-    };
+  const toggleTheme = () =>
+    setTheme(theme === themes.dark ? themes.light : theme.dark);
 
-    this.state = {
-      theme: themes.light,
-      toggleTheme: this.toggleTheme,
-    };
-  }
-  render() {
-    const contentClass =
-      this.state.theme === themes.dark
-        ? "app__content app__content_dark"
-        : "app__content";
-    return (
-      <div className="app">
-        <ThemeContext.Provider value={this.state}>
-          <Router basename="/react-app">
-            <Header />
-            <div className={contentClass}>
-              <ErrorBoundary>
-                <ToDoListPage />
-              </ErrorBoundary>
-              <Redirect to="./" />
-            </div>
-          </Router>
-        </ThemeContext.Provider>
-      </div>
-    );
-  }
-}
-
-export default App;
+  const contentClass =
+    theme === themes.dark ? "app__content app__content_dark" : "app__content";
+  return (
+    <div className="app">
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <Router basename="/react-app">
+          <Header />
+          <div className={contentClass}>
+            <ErrorBoundary>
+              <ToDoListPage />
+            </ErrorBoundary>
+            <Redirect to="/" />
+          </div>
+        </Router>
+      </ThemeContext.Provider>
+    </div>
+  );
+};
