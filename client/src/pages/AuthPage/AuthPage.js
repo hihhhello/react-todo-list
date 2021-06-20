3fd5bfd437b51211b11574a80ca3b167cf85a942
hihhhello <grigori.ratushniak@gmail.com>
2021-06-20 22:01:13 +0300

    Added test implementation of AuthPage

---
+++ client/src/pages/AuthPage/AuthPage.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { useScript } from "../../hooks/script.hooks";
+import "./_auth-page.sass";
+
+
+export const AuthPage = () => {
+  useScript({
+    url: "https://telegram.org/js/telegram-widget.js?15",
+    isAsync: true,
+    data: [
+      { attr: "telegramLogin", value: "node321bot" },
+      { attr: "size", value: "large" },
+      { attr: "authUrl", value: "/react-app/api/tg-auth" },
+      { attr: "requestAccess", value: "write" },
+    ],
+    parentSelector: ".auth-page__wrapper",
+  });
+
+  
+
+  return (
+    <div className="auth-page">
+      <div className="container">
+        <div className="auth-page__wrapper">
+          <h1 className="auth-page__title">
+            Synchronize your table from{" "}
+            <a href="https://t.me/node321bot" target="_blank" rel="noreferrer">
+              telegram bot
+            </a>
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
