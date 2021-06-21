fbe232bb258a683cd85593468d012c3b2766ff08
hihhhello <grigori.ratushniak@gmail.com>
2021-06-21 23:36:21 +0300

    Fixed import

--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
 
 export const LoginPage = () => {
+    console.log(useParams);
   return <h1>Login Page</h1>;
 };
--- client/src/pages/LoginPage/index.js
+++ client/src/pages/LoginPage/index.js
@@ -1,2 +1,2 @@
-export { LoginPage } from "./LoginPage";
+import { LoginPage } from "./LoginPage.js";
 export default LoginPage;
